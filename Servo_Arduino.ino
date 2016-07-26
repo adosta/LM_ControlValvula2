eef35efc696a246d783bb4446887544aa03f0244
adosta <adrianboy127@hotmail.com>
2016-07-26 17:43:13 -0600

    Add files via upload
    Archivo de Arduino para control del servo

---
+++ Servo_Arduino.ino
@@ -0,0 +1,26 @@
+#include <Servo.h>
+
+Servo servoMotor;
+int servoPin=13;
+int value=0;
+int angle;
+int x;
+
+void setup(){
+  Serial.begin(9600);
+  servoMotor.attach(servoPin);
+}
+
+void loop(){
+  if(Serial.available() >0) 
+  {
+    value = Serial.read();
+    if(value>0){
+      x=value;
+    }
+  }
+  else if (x>=0 && x<=180){
+    angle=x;
+    servoMotor.write(angle); 
+  }
+}
